5cc691b1d5abcfa3e6647d8b95289e3a79ea910f
Christian <christian.salazar@doublevpartners.com-at-280831901412>
2023-11-18 08:17:25 -0500

    Se realizan los ajustes para los modelos, factory, Seeder

--- app/Models/Pedido.php
+++ app/Models/Pedido.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Pedido extends Model
 {
     use HasFactory;
+    protected $fillable = ['idCuenta', 'product', 'amount','value','total'];
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Cuenta;
+use App\Models\Pedido;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +15,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Cuenta::factory()->count(3)->create();
+        Pedido::factory()->count(1)->create([
+            'product'=>'Cafe',
+            'amount'=>2,
+            'value'=>2000,
+            'total'=>4000,
+        ]);
+        Pedido::factory()->count(1)->create([
+            'product'=>'torta',
+            'amount'=>2,
+            'value'=>3000,
+            'total'=>6000,
+        ]);
     }
 }
